0b48b29d5f2163b58be88eb5f4004d00c5fbafad
KwanKreative <kwan_sqn@hotmail.com>
2023-03-01 22:29:08 -0500

    js

--- lightbox/js/lightbox.js
+++ lightbox/js/lightbox.js
@@ -2,7 +2,7 @@ let lightboxContainer = document.getElementById("lightboxContainer");
 let lightboxBackground = document.getElementById("lightboxBackground");
 let lightbox = document.getElementById("lightbox");
 let lightboxCloser = document.getElementById("lightboxCloser");
-
+//Image 1 
 function openLightbox(){
     lightboxContainer.classList.add("display");
     lightbox.src = this.src;
@@ -10,6 +10,154 @@ function openLightbox(){
 
 let coffeeImage = document.getElementById("coffeeImage");
 coffeeImage.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+
+//Image 2 
+
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage2 = document.getElementById("coffeeImage2");
+coffeeImage2.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+
+//Image 3 
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage3 = document.getElementById("coffeeImage3");
+coffeeImage3.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+
+//Image 4 
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage4 = document.getElementById("coffeeImage4");
+coffeeImage4.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+//Image 5 
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage5 = document.getElementById("coffeeImage5");
+coffeeImage5.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+//Image 6
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage6 = document.getElementById("coffeeImage6");
+coffeeImage6.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+//Image 7
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage7 = document.getElementById("coffeeImage7");
+coffeeImage7.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+//Image 8
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage8 = document.getElementById("coffeeImage8");
+coffeeImage8.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+//Image 9
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage9 = document.getElementById("coffeeImage9");
+coffeeImage9.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+//Image 10
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImag10 = document.getElementById("coffeeImage10");
+coffeeImage10.onclick = openLightbox;
 
 function closeLightbox() {
     lightboxContainer.classList.remove("display");
