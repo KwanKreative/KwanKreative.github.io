383290cbb1e92131fdbc274ab4f7621a6c0790cb
KwanKreative <kwan_sqn@hotmail.com>
2023-03-01 19:10:18 -0500

    j

---
+++ calculator/js/calculator.js
@@ -0,0 +1,15 @@
+let result = document.getElementById("result");
+
+function displayTwoPlusTwo() {
+    let number1 = 2;
+    let number2 = 2;
+    result.innerHTML = number1 + number2;
+}
+
+function displayFourTimesTwo() {
+    result.innerHTML = 4 * 2;
+}
+
+function displayOneHundreddividedByTwo() {
+    result.innerHTML = 100 / 2
+}
