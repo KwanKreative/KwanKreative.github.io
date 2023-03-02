0debfbb77b387584240936409c78702eece19723
KwanKreative <kwan_sqn@hotmail.com>
2023-03-01 20:54:07 -0500

    cal

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -20,3 +20,9 @@ function displayMultiply(){
     result.innerHTML = input1.value * input2.value;
 }
 
+function displayAddition(){
+    let input1 = document.getElementById("input1");
+    let input2 = document.getElementById("input2");
+    result.innerHTML = 
+        parseInt(input1.value) +  parseInt(input2.value);
+}
