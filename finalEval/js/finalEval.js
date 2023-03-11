b5abdf715bc13ed64ce3a0b83781efd65736668c
KwanKreative <kwan_sqn@hotmail.com>
2023-03-10 21:56:02 -0500

    ffaa

--- finalEval/js/finalEval.js
+++ finalEval/js/finalEval.js
@@ -1,35 +1,32 @@
 const flexbox2 = document.getElementById("flexbox2");
 const flexbox3 = document.getElementById("flexbox3");
-const grid= document.getElementById("grid");
+const grid = document.getElementById("grid");
 const heroLink = document.getElementById("heroLink");
-const flexboxGridLink = document.getElementById("flexboxGridLink");
+const flexboxGridLink =
+    document.getElementById("flexboxGridLink");
 const gridLink = document.getElementById("gridLink");
 
 function hideAll() {
-    // flexbox2.style.display = "none";
+    flexbox2.style.display = "none";
     flexbox3.style.display = "none";
     grid.style.display = "none";
 }
-
 hideAll();
 
-function showHero(){
+function showHero() {
     hideAll();
     flexbox2.style.display = "";
 }
-
 heroLink.onclick = showHero;
 
-function showFlexboxGrid(){
+function showFlexboxGrid() {
     hideAll();
     flexbox3.style.display = "";
 }
-
 flexboxGridLink.onclick = showFlexboxGrid;
 
-function showGrid(){
+function showGrid() {
     hideAll();
     grid.style.display = "";
 }
-
 gridLink.onclick = showGrid;
